751fb35b8dd958bc19299f646b691056668eb974
Joyner, Scott <kipnerter@gmail.com>
2025-08-27 13:43:00 -0400

    Added files

--- graph-alpha-bot/app/settings.py
+++ graph-alpha-bot/app/settings.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
-NEO4J_USER = os.getenv("NEO4J_USER", "")
+NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
 NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "")
 
 SNAPTRADE_CLIENT_ID = os.getenv("SNAPTRADE_CLIENT_ID", "")
